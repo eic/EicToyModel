b59f5bab119968663077ba51e277d17245a3e62d
Alexander Kiselev <ayk@bnl.gov>
2020-07-15 08:44:40 -0400

    EicRoot-induced bug fix in etmLinkDef.h

--- include/etmLinkDef.h
+++ include/etmLinkDef.h
@@ -5,6 +5,48 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+//#pragma link C++ namespace eic;
+
+#pragma link C++ class EicDetName+;
+
+#pragma link C++ class EicNamePatternHub<unsigned>+;
+#pragma link C++ class EicNamePatternHub<double>+;
+//#pragma link C++ class EicNamePatternHub<SteppingType>+;
+//#pragma link C++ class std::pair<TString,SteppingType>+;
+#pragma link C++ class std::pair<TString,unsigned>+;
+#pragma link C++ class EicNamePatternHub<Color_t>+;
+#pragma link C++ class EicNamePatternHub<Char_t>+;
+#pragma link C++ class std::pair<TString,Color_t>+;
+#pragma link C++ class std::pair<TString,Char_t>+;
+
+#pragma link C++ class std::pair<TString,Color_t>+;
+#pragma link C++ class std::pair<TString,Char_t>+;
+#pragma link C++ class EicBitMask<UGeantIndex_t>+;
+#pragma link C++ class EicBitMask<ULogicalIndex_t>+;
+#pragma link C++ class GeantVolumeLevel+;
+#pragma link C++ class EicGeoMap+;
+#pragma link C++ class SourceFile+;
+#pragma link C++ class LogicalVolumeGroupProjection+;
+#pragma link C++ class LogicalVolumeGroup+;
+#pragma link C++ class EicGeoParData+;
+#pragma link C++ class LogicalVolumeLookupTableEntry+;
+#pragma link C++ class std::pair<ULong64_t,ULong64_t>+;
+#pragma link C++ class std::pair<TString, Int_t>+;
+
+#pragma link C++ class GemModule+;
+#pragma link C++ class GemWheel+;
+#pragma link C++ class GemGeoParData+;
+#pragma link C++ class MuMegasLayer+;
+#pragma link C++ class MuMegasBarrel+;
+#pragma link C++ class MuMegasGeoParData+;
+#pragma link C++ class MapsGeoParData+;
+//#pragma link C++ class VstGeoParData+;
+//#pragma link C++ class FstGeoParData+;
+//#pragma link C++ class FstDisc+;
+//#pragma link C++ class VstBarrelLayer+;
+#pragma link C++ class MapsStave+;
+#pragma link C++ class MapsMimosaAssembly+;
+
 #pragma link C++ namespace etm;
 
 #pragma link C++ class EtmLine2D+;
